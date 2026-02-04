7ed805813bbc968a1055cb58ee5d9adcfc0b5c70
seanrw93 <seanrw@gmail.com>
2026-02-04 18:16:21 +0100

    Add alternative layout file to ensure google metadata verification

---
+++ app/layout.tsx
@@ -0,0 +1,17 @@
+// Alterative layout file to ensure Google metadata verification
+
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  verification: {
+    google: "DaKsCzPVKKWbJ8svTpyqJruKDoE1tPqNXXm4Sc35XpQ",
+  },
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
